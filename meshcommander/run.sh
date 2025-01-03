47afb1ce6425757bee30f1a52f91202abda98624
LinusT <4124185+LinusT@users.noreply.github.com>
2025-01-03 22:09:35 +0100

    New fix for ingress routing

--- meshcommander/run.sh
+++ meshcommander/run.sh
@@ -4,4 +4,4 @@
 echo "Starting MeshCommander for Ingress..."
 
 # Start MeshCommander and bind to localhost for Ingress
-exec meshcommander --listen 127.0.0.1:3000
+exec meshcommander --listen 127.0.0.1:3000 --basepath /
